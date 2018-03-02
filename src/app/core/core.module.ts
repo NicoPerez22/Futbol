7491559de79f8ef2d9e82fdd3dcbac07ca821fd5
Nico Perez <nicoperez2295@gmail.com>
2018-03-02 13:54:15 -0300

    Cambios html, vistas semi terminadas

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell/shell.component';
 import { FooterComponent } from './shell/footer/footer.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { SideBarComponent } from './shell/side-bar/side-bar.component';
 import { ContentComponent } from './shell/content/content.component';
+import { CategoriasComponent } from '../categorias/categorias.component';
+import { SponsorComponent } from '../sponsor/sponsor.component';
+import { GastosgeneralesComponent } from '../gastosgenerales/gastosgenerales.component';
+import { JugadorComponent } from '../jugador/jugador.component';
+import { TelefonosutilesComponent } from '../telefonosutiles/telefonosutiles.component';
+import { ArbitrosmedicosComponent } from '../arbitrosmedicos/arbitrosmedicos.component';
+import { GastomicroComponent } from '../gastomicro/gastomicro.component';
+import { IngresopartidoComponent } from '../ingresopartido/ingresopartido.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ShellComponent, FooterComponent, HeaderComponent, SideBarComponent, ContentComponent],
+  declarations: [ShellComponent, 
+    FooterComponent, 
+    HeaderComponent, 
+    SideBarComponent, 
+    ContentComponent, 
+    CategoriasComponent, 
+    SponsorComponent, 
+    JugadorComponent, 
+    TelefonosutilesComponent,
+    ArbitrosmedicosComponent, 
+    GastomicroComponent, 
+    GastosgeneralesComponent,
+    IngresopartidoComponent],
+    
   exports: [ShellComponent]
 })
 export class CoreModule { }
--- src/app/core/shell/content/content.component.ts
+++ src/app/core/shell/content/content.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+    // lineChart
+    public lineChartData: Array<any> = [
+      {data: [65, 59, 80, 81, 56, 55, 40], label: 'Pagos Jugadores'},
+      {data: [28, 48, 40, 19, 86, 27, 90], label: 'Sponsors'},
+      {data: [18, 48, 77, 9, 100, 27, 40], label: 'Ingresos Partidos'}
+    ];
+    public lineChartLabels: Array<any> = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+    public lineChartOptions: any = {
+      responsive: true
+    };
+    
 
   constructor() { }
 
